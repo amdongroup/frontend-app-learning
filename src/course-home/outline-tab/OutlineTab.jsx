790491580ce935343211a38f20205d087a9a6e33
aungphyooo <aungphyooo@amdon.com>
2022-05-04 14:08:01 +0630

    primary

--- src/course-home/outline-tab/OutlineTab.jsx
+++ src/course-home/outline-tab/OutlineTab.jsx
@@ -1,75 +1,62 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { sendTrackEvent, sendTrackingLogEvent } from '@edx/frontend-platform/analytics';
-import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  sendTrackEvent,
+  sendTrackingLogEvent,
+} from "@edx/frontend-platform/analytics";
+import { getAuthenticatedUser } from "@edx/frontend-platform/auth";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
 
-import { Button, Toast } from '@edx/paragon';
-import { AlertList } from '../../generic/user-messages';
+import { Button, Toast } from "@edx/paragon";
+import { AlertList } from "../../generic/user-messages";
 
-import CourseDates from './widgets/CourseDates';
-import CourseGoalCard from './widgets/CourseGoalCard';
-import CourseHandouts from './widgets/CourseHandouts';
-import CourseTools from './widgets/CourseTools';
-import { fetchOutlineTab } from '../data';
-import genericMessages from '../../generic/messages';
-import messages from './messages';
-import Section from './Section';
-import ShiftDatesAlert from '../suggested-schedule-messaging/ShiftDatesAlert';
-import UpdateGoalSelector from './widgets/UpdateGoalSelector';
-import UpgradeNotification from '../../generic/upgrade-notification/UpgradeNotification';
-import UpgradeToShiftDatesAlert from '../suggested-schedule-messaging/UpgradeToShiftDatesAlert';
-import useCertificateAvailableAlert from './alerts/certificate-status-alert';
-import useCourseEndAlert from './alerts/course-end-alert';
-import useCourseStartAlert from '../../alerts/course-start-alert';
-import usePrivateCourseAlert from './alerts/private-course-alert';
-import useScheduledContentAlert from './alerts/scheduled-content-alert';
-import { useModel } from '../../generic/model-store';
-import WelcomeMessage from './widgets/WelcomeMessage';
-import ProctoringInfoPanel from './widgets/ProctoringInfoPanel';
-import AccountActivationAlert from '../../alerts/logistration-alert/AccountActivationAlert';
+import CourseDates from "./widgets/CourseDates";
+import CourseGoalCard from "./widgets/CourseGoalCard";
+import CourseHandouts from "./widgets/CourseHandouts";
+import CourseTools from "./widgets/CourseTools";
+import { fetchOutlineTab } from "../data";
+import genericMessages from "../../generic/messages";
+import messages from "./messages";
+import Section from "./Section";
+import ShiftDatesAlert from "../suggested-schedule-messaging/ShiftDatesAlert";
+import UpdateGoalSelector from "./widgets/UpdateGoalSelector";
+import UpgradeNotification from "../../generic/upgrade-notification/UpgradeNotification";
+import UpgradeToShiftDatesAlert from "../suggested-schedule-messaging/UpgradeToShiftDatesAlert";
+import useCertificateAvailableAlert from "./alerts/certificate-status-alert";
+import useCourseEndAlert from "./alerts/course-end-alert";
+import useCourseStartAlert from "../../alerts/course-start-alert";
+import usePrivateCourseAlert from "./alerts/private-course-alert";
+import useScheduledContentAlert from "./alerts/scheduled-content-alert";
+import { useModel } from "../../generic/model-store";
+import WelcomeMessage from "./widgets/WelcomeMessage";
+import ProctoringInfoPanel from "./widgets/ProctoringInfoPanel";
+import AccountActivationAlert from "../../alerts/logistration-alert/AccountActivationAlert";
 
 /** [MM-P2P] Experiment */
-import { initHomeMMP2P, MMP2PFlyover } from '../../experiments/mm-p2p';
+import { initHomeMMP2P, MMP2PFlyover } from "../../experiments/mm-p2p";
 
 function OutlineTab({ intl }) {
-  const {
-    courseId,
-  } = useSelector(state => state.courseHome);
+  const { courseId } = useSelector((state) => state.courseHome);
 
-  const {
-    isSelfPaced,
-    org,
-    title,
-    username,
-    userTimezone,
-  } = useModel('courseHomeMeta', courseId);
+  const { isSelfPaced, org, title, username, userTimezone } = useModel(
+    "courseHomeMeta",
+    courseId
+  );
 
   const {
     accessExpiration,
-    courseBlocks: {
-      courses,
-      sections,
-    },
-    courseGoals: {
-      goalOptions,
-      selectedGoal,
-    } = {},
+    courseBlocks: { courses, sections },
+    courseGoals: { goalOptions, selectedGoal } = {},
     datesBannerInfo,
-    datesWidget: {
-      courseDateBlocks,
-    },
-    resumeCourse: {
-      hasVisitedCourse,
-      url: resumeCourseUrl,
-    },
+    datesWidget: { courseDateBlocks },
+    resumeCourse: { hasVisitedCourse, url: resumeCourseUrl },
     offer,
     timeOffsetMillis,
     verifiedMode,
-  } = useModel('outline', courseId);
+  } = useModel("outline", courseId);
 
   const [courseGoalToDisplay, setCourseGoalToDisplay] = useState(selectedGoal);
-  const [goalToastHeader, setGoalToastHeader] = useState('');
+  const [goalToastHeader, setGoalToastHeader] = useState("");
   const [expandAll, setExpandAll] = useState(false);
 
   const eventProperties = {
@@ -78,9 +65,9 @@ function OutlineTab({ intl }) {
   };
 
   const logResumeCourseClick = () => {
-    sendTrackingLogEvent('edx.course.home.resume_course.clicked', {
+    sendTrackingLogEvent("edx.course.home.resume_course.clicked", {
       ...eventProperties,
-      event_type: hasVisitedCourse ? 'resume' : 'start',
+      event_type: hasVisitedCourse ? "resume" : "start",
       url: resumeCourseUrl,
     });
   };
@@ -94,48 +81,64 @@ function OutlineTab({ intl }) {
 
   const rootCourseId = courses && Object.keys(courses)[0];
 
-  const hasDeadlines = courseDateBlocks && courseDateBlocks.some(x => x.dateType === 'assignment-due-date');
+  const hasDeadlines =
+    courseDateBlocks &&
+    courseDateBlocks.some((x) => x.dateType === "assignment-due-date");
 
   const logUpgradeToShiftDatesLinkClick = () => {
-    sendTrackEvent('edx.bi.ecommerce.upsell_links_clicked', {
+    sendTrackEvent("edx.bi.ecommerce.upsell_links_clicked", {
       ...eventProperties,
-      linkCategory: 'personalized_learner_schedules',
-      linkName: 'course_home_upgrade_shift_dates',
-      linkType: 'button',
-      pageName: 'course_home',
+      linkCategory: "personalized_learner_schedules",
+      linkName: "course_home_upgrade_shift_dates",
+      linkType: "button",
+      pageName: "course_home",
     });
   };
 
   const isEnterpriseUser = () => {
     const authenticatedUser = getAuthenticatedUser();
-    const userRoleNames = authenticatedUser ? authenticatedUser.roles.map(role => role.split(':')[0]) : [];
+    const userRoleNames = authenticatedUser
+      ? authenticatedUser.roles.map((role) => role.split(":")[0])
+      : [];
 
-    return userRoleNames.includes('enterprise_learner');
+    return userRoleNames.includes("enterprise_learner");
   };
 
   /** [[MM-P2P] Experiment */
   const MMP2P = initHomeMMP2P(courseId);
 
   /** show post enrolment survey to only B2C learners */
-  const learnerType = isEnterpriseUser() ? 'enterprise_learner' : 'b2c_learner';
+  const learnerType = isEnterpriseUser() ? "enterprise_learner" : "b2c_learner";
 
   return (
     <>
       <Toast
         closeLabel={intl.formatMessage(genericMessages.close)}
-        onClose={() => setGoalToastHeader('')}
-        show={!!(goalToastHeader)}
+        onClose={() => setGoalToastHeader("")}
+        show={!!goalToastHeader}
       >
         {goalToastHeader}
       </Toast>
-      <div data-learner-type={learnerType} className="row w-100 mx-0 my-3 justify-content-between">
+      <div
+        data-learner-type={learnerType}
+        className="row w-100 mx-0 my-3 justify-content-between"
+      >
         <div className="col-12 col-sm-auto p-0">
-          <div role="heading" aria-level="1" className="h2">{title}</div>
+          <div role="heading" aria-level="1" className="h2">
+            {title}
+          </div>
         </div>
         {resumeCourseUrl && (
           <div className="col-12 col-sm-auto p-0">
-            <Button variant="brand" block href={resumeCourseUrl} onClick={() => logResumeCourseClick()}>
-              {hasVisitedCourse ? intl.formatMessage(messages.resume) : intl.formatMessage(messages.start)}
+            <Button
+              variant="primary"
+              block
+              href={resumeCourseUrl}
+              onClick={() => logResumeCourseClick()}
+            >
+              {hasVisitedCourse
+                ? intl.formatMessage(messages.resume)
+                : intl.formatMessage(messages.start)}
             </Button>
           </div>
         )}
@@ -152,9 +155,8 @@ function OutlineTab({ intl }) {
           />
         </div>
         <div className="col col-12 col-md-8">
-          { /** [MM-P2P] Experiment (the conditional) */ }
-          { !MMP2P.state.isEnabled
-            && (
+          {/** [MM-P2P] Experiment (the conditional) */}
+          {!MMP2P.state.isEnabled && (
             <AlertList
               topic="outline-course-alerts"
               className="mb-3"
@@ -165,11 +167,14 @@ function OutlineTab({ intl }) {
                 ...scheduledContentAlert,
               }}
             />
-            )}
+          )}
           {isSelfPaced && hasDeadlines && !MMP2P.state.isEnabled && (
             <>
               <ShiftDatesAlert model="outline" fetch={fetchOutlineTab} />
-              <UpgradeToShiftDatesAlert model="outline" logUpgradeLinkClick={logUpgradeToShiftDatesLinkClick} />
+              <UpgradeToShiftDatesAlert
+                model="outline"
+                logUpgradeLinkClick={logUpgradeToShiftDatesLinkClick}
+              />
             </>
           )}
           {!courseGoalToDisplay && goalOptions && goalOptions.length > 0 && (
@@ -177,8 +182,12 @@ function OutlineTab({ intl }) {
               courseId={courseId}
               goalOptions={goalOptions}
               title={title}
-              setGoalToDisplay={(newGoal) => { setCourseGoalToDisplay(newGoal); }}
-              setGoalToastHeader={(newHeader) => { setGoalToastHeader(newHeader); }}
+              setGoalToDisplay={(newGoal) => {
+                setCourseGoalToDisplay(newGoal);
+              }}
+              setGoalToastHeader={(newHeader) => {
+                setGoalToastHeader(newHeader);
+              }}
             />
           )}
           <WelcomeMessage courseId={courseId} />
@@ -186,8 +195,16 @@ function OutlineTab({ intl }) {
             <>
               <div className="row w-100 m-0 mb-3 justify-content-end">
                 <div className="col-12 col-sm-auto p-0">
-                  <Button variant="outline-primary" block onClick={() => { setExpandAll(!expandAll); }}>
-                    {expandAll ? intl.formatMessage(messages.collapseAll) : intl.formatMessage(messages.expandAll)}
+                  <Button
+                    variant="outline-primary"
+                    block
+                    onClick={() => {
+                      setExpandAll(!expandAll);
+                    }}
+                  >
+                    {expandAll
+                      ? intl.formatMessage(messages.collapseAll)
+                      : intl.formatMessage(messages.expandAll)}
                   </Button>
                 </div>
               </div>
@@ -207,47 +224,46 @@ function OutlineTab({ intl }) {
         </div>
         {rootCourseId && (
           <div className="col col-12 col-md-4">
-            <ProctoringInfoPanel
-              courseId={courseId}
-              username={username}
-            />
+            <ProctoringInfoPanel courseId={courseId} username={username} />
             {courseGoalToDisplay && goalOptions && goalOptions.length > 0 && (
               <UpdateGoalSelector
                 courseId={courseId}
                 goalOptions={goalOptions}
                 selectedGoal={courseGoalToDisplay}
-                setGoalToDisplay={(newGoal) => { setCourseGoalToDisplay(newGoal); }}
-                setGoalToastHeader={(newHeader) => { setGoalToastHeader(newHeader); }}
+                setGoalToDisplay={(newGoal) => {
+                  setCourseGoalToDisplay(newGoal);
+                }}
+                setGoalToastHeader={(newHeader) => {
+                  setGoalToastHeader(newHeader);
+                }}
               />
             )}
-            <CourseTools
-              courseId={courseId}
-            />
-            { /** [MM-P2P] Experiment (conditional) */ }
-            { MMP2P.state.isEnabled
-              ? <MMP2PFlyover isStatic options={MMP2P} />
-              : (
-                <UpgradeNotification
-                  offer={offer}
-                  verifiedMode={verifiedMode}
-                  accessExpiration={accessExpiration}
-                  contentTypeGatingEnabled={datesBannerInfo.contentTypeGatingEnabled}
-                  upsellPageName="course_home"
-                  userTimezone={userTimezone}
-                  shouldDisplayBorder
-                  timeOffsetMillis={timeOffsetMillis}
-                  courseId={courseId}
-                  org={org}
-                />
-              )}
+            <CourseTools courseId={courseId} />
+            {/** [MM-P2P] Experiment (conditional) */}
+            {MMP2P.state.isEnabled ? (
+              <MMP2PFlyover isStatic options={MMP2P} />
+            ) : (
+              <UpgradeNotification
+                offer={offer}
+                verifiedMode={verifiedMode}
+                accessExpiration={accessExpiration}
+                contentTypeGatingEnabled={
+                  datesBannerInfo.contentTypeGatingEnabled
+                }
+                upsellPageName="course_home"
+                userTimezone={userTimezone}
+                shouldDisplayBorder
+                timeOffsetMillis={timeOffsetMillis}
+                courseId={courseId}
+                org={org}
+              />
+            )}
             <CourseDates
               courseId={courseId}
               /** [MM-P2P] Experiment */
               mmp2p={MMP2P}
             />
-            <CourseHandouts
-              courseId={courseId}
-            />
+            <CourseHandouts courseId={courseId} />
           </div>
         )}
       </div>
