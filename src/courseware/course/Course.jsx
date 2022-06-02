0f0a4b5909174643a0f30c9aad367d742f5fb0e9
aungphyooo <aungphyooo@amdon.com>
2022-06-02 09:31:56 +0630

    remove cmt

--- src/courseware/course/Course.jsx
+++ src/courseware/course/Course.jsx
@@ -302,7 +302,7 @@ function Course({
         ) : null}
       </div>
       {
-        // available_cert_id && !seenBox && 
+        available_cert_id && !seenBox && 
         <CertificateReceiveAlert  
         availableCertId={available_cert_id}
           courseId={courseId}
