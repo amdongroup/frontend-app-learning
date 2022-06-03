7200a5ccdee7f56fbff69c5c0cb24a59f4f97a22
aungphyooo <aungphyooo@amdon.com>
2022-06-03 13:17:59 +0630

    consider as recall when progress_data changed

--- src/courseware/course/CertificateReceiveAlert.jsx
+++ src/courseware/course/CertificateReceiveAlert.jsx
@@ -67,7 +67,7 @@ function CertificateReceiveAlert({
     isChecked()
     // call check api whether to show pass or change grade
 
-  },[]);
+  },[progress_data]);
 
   return (
    <div className="alert-wrapper" id="certificate-receive-alert" >
